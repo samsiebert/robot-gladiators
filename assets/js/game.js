b6524abb3bcfee54a72bbeaf04e3b411a4bf9436
samsiebert <sammsiebert@gmail.com>
2022-05-23 12:38:40 -0500

    shop updates

--- assets/js/game.js
+++ assets/js/game.js
@@ -8,19 +8,33 @@ var randomNumber = function(min, max) {
 
 // shop function
 var shop = function() {
-    console.log("entered the shop");
-    playerInfo.health = playerInfo.health + 20;
-    playerInfo.money = playerInfo.money -7;
+    // ask player what they'd like to do
+    var shopOptionPrompt = window.prompt(
+        "Would you like to REFILL your helath, UPGRADE your attack, or LEAVE the store? Please enter one "REFILL", "UPGRADE", or "LEAVE" to make a choice."
+    );
 
-    // case "REFILL" :
-    // case "refill":
-    //     playerInfo.refillHealth();
-    //     break;
-    // case "UPGRADE":
-    // case "upgrade":
-    //     playerInfor.upgradeAttack();
-    //     break;
-};
+    // use switch case to carry out action
+    switch (shopOptionPrompt) {
+        case "REFILL" :
+        case "refill":
+            playerInfo.refillHealth();
+            break;
+        case "UPGRADE":
+        case "upgrade":
+            playerInfor.upgradeAttack();
+            break;
+        case "LEAVE":
+        case "leave":
+            window.alert("Leaving the store.");
+        // do nothing, so function will end
+        break;
+        default:
+            window.alert("You did not pick a valid option. Try again.");
+            //call shop() again to force player to pick a valid option
+            shop();
+            break;
+        }
+    };
 
 // player info onject
 var playerInfo = {
